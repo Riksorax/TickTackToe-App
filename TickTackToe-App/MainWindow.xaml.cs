45f577000181bab5c24b11962c32a408bbbc85da
Riksorax <78565646+Riksorax@users.noreply.github.com>
2021-02-23 10:18:55 +0100

    Es ist/war ansich einfach alles so einfach, ich hätte einfach nur nicht so kompli denken sollen. Ich habe ob im Klick Event angefangen das überprüft ob das Spielfeld voll ist oder nicht. Danach kommt ein if das überprüft ob das Kästchen /  der Button schon belegt ist oder nicht, wenn ja bekommt man einen Hinweis. Danach wird defeniert wenn der erste Spieler drann ist wird ein X gesetzt ansonsten wird ein O gesetzt. Dann kommt einmal die Methode  zur Überprüfung vom vollem Spielfeld. Und dann kommt nur noch das einmal alle Kästchcen geleert werden

--- TickTackToe-App/MainWindow.xaml.cs
+++ TickTackToe-App/MainWindow.xaml.cs
@@ -24,11 +24,31 @@ namespace TickTackToe_App
         private bool _istErsterSpielerAmZug = true;
 
 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
+       
         private void Kaestchen_Click(object sender, RoutedEventArgs e)
         {
             Button geklickterButton = (Button)sender;
 
+            // Überprufng vom Spielfeld voll ja oder nein
+            if (IstSpielfeldVoll())
+            {
+                Spielfeldleeren();
+                _istErsterSpielerAmZug = true;
+            }
+
+            // Wenn ein Kästenchen belegt ist bekommt man eine Hinweis
+            if (geklickterButton.Content != null && geklickterButton.Content.ToString() != "")
+            {
+                MessageBox.Show("Kästchen ist bereits belegt, versuch ein anderes!!!");
+                return;
+            }
+
+            // Es wird hier definert wenn der erste Spieler am zug ist wird ein  X gestzt ansonsten wird ein O gesetzt
             if (_istErsterSpielerAmZug)
             {
                 geklickterButton.Content = "X";
@@ -39,6 +59,45 @@ namespace TickTackToe_App
                 geklickterButton.Content = "O";
                 _istErsterSpielerAmZug = true;
             }
+
+            
+        }
+
+        // Hier wird überprüft ob das Spielfeld von unten voll ist wenn ja einmal leeren und wenn nicht, passier nix
+        private bool IstSpielfeldVoll()
+        {
+            foreach (var item in Spielfeld.Children)
+            {
+                Button keastchen = item as Button;
+
+                if(keastchen != null && keastchen.Content == string.Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Hier wird das Grid angesprochen und geleert
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Spielfeldleeren();
+        }
+
+        // Hier wird jeder einzelne Button geleert
+        private void Spielfeldleeren()
+        {
+            bt1_0_0.Content = string.Empty;
+            bt2_0_1.Content = string.Empty;
+            bt3_0_2.Content = string.Empty;
+
+            bt4_1_0.Content = string.Empty;
+            bt5_1_1.Content = string.Empty;
+            bt6_1_2.Content = string.Empty;
+
+            bt7_2_0.Content = string.Empty;
+            bt8_2_1.Content = string.Empty;
+            bt9_2_2.Content = string.Empty;
         }
     }
 }
